bd1b2808d767accb2541f45d32f91cebaff727b8
TommasoAzz <azzalintommaso@gmail.com>
2021-09-27 22:16:11 +0200

    Aggiornato

--- context_aware/contextaware/__main__.py
+++ context_aware/contextaware/__main__.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
-from context_aware.contextaware.classifier.classifier import Classifier
-from flask import Flask, request, jsonify
+from context_aware.contextaware.classifier.classifier import ActivityClassifier
+from flask import Flask, request
 from datetime import datetime
 import pandas as pd
-from os.path import join
-from model_utility import model
 
 app = Flask(__name__)
+classifier = ActivityClassifier.get_instance()
 
 @app.route("/recommendation/places", methods=["GET"])
 def recommend_places():
@@ -15,6 +14,8 @@ def recommend_places():
     Returns the recommended places (points of interest) to visit at the given parameters found as arguments.
     """
     # Arguments
+    latitude = request.args.get('latitude')
+    longitude = request.args.get('longitude')
     human_activity = request.args.get('human_activity')
     date_time = request.args.get('date_time')
 
@@ -24,11 +25,13 @@ def recommend_places():
     seconds = (date_time.hour * 3600) + (date_time.minute * 60) + date_time.second
     # 0 = Monday, 6 = Sunday
     day_of_week = date_time.weekday()
-    # time_of_day,day_of_week,har
-    new_record = pd.DataFrame(columns=["time_of_day", "day_of_week", "har"])
-    new_record.loc[0] = [seconds, day_of_week, human_activity]
     
-    predicted_activity = Classifier.predict(new_record)
+    # New record creation
+    new_record = pd.DataFrame(columns=["latitude", "longitude", "time_of_day", "day_of_week", "human_activity"])
+    new_record.loc[0] = [latitude, longitude, seconds, day_of_week, human_activity]
+    
+    # Prediction
+    predicted_activity = classifier.predict(new_record)
     json_res = {
         "category_response": predicted_activity
     }
@@ -51,9 +54,4 @@ def recommend_places():
 #     pass
 
 if __name__ == "__main__":
-    # Instanciating the singleton Classifier.
-    Classifier(
-        cached_model_filename = join('classifier', 'model.sav'),
-        train_dataset_filename = join('classifier', 'dataset.csv')
-    )
     app.run("0.0.0.0", 4000)
--- context_aware/contextaware/classifier/classifier.py
+++ context_aware/contextaware/classifier/classifier.py
@@ -1,34 +1,51 @@
 from _typeshed import OpenTextMode
 import pandas as pd
 import numpy as np
+from os.path import join
+from pandas.core.indexes.base import Index
 from sklearn.tree import DecisionTreeClassifier
+from __future__ import annotations
+from typing import Tuple
 import pickle
 
-class Classifier:
+class ActivityClassifier:
     class __Classifier:
-        def __init__(self, cached_model_filename: str, train_dataset_filename: str) -> None:
-            self.cached_model_filename: str = cached_model_filename
-            self.train_dataset_filename: str = train_dataset_filename
+        def __init__(self) -> None:
+            self.cached_model_filename: str = join('contextaware', 'classifier', 'model.sav'),
+            self.train_dataset_filename: str = join('contextaware', 'classifier', 'dataset.csv'),
 
-            self.update_state()
-        
-        def update_state(self):
             self.train_dataset: pd.DataFrame = pd.read_csv(self.train_dataset_filename)
             self.model: DecisionTreeClassifier = pickle.load(open(self.cached_model_filename, mode='rb'))
     
-    __instance = None
-    targets_list = ("leisure", "restaurants", "sport")
+    __instance: __Classifier = None
+    """
+    Instance of inner class __Classifier.
+    """
 
-    def __init__(self, cached_model_filename: str, train_dataset_filename: str) -> None:
-        self.cached_model_filename: str = cached_model_filename
-        self.train_dataset_filename: str = train_dataset_filename
-        
-        if not Classifier.__instance:
-            Classifier.__instance = Classifier.__Classifier(cached_model_filename, train_dataset_filename)
-        else:
-            Classifier.__instance.cached_model_filename = cached_model_filename
-            Classifier.__instance.train_dataset_filename = train_dataset_filename
-            Classifier.__instance.update_state()
+    targets_list: Tuple[str] = ("leisure", "restaurants", "sport")
+    """
+    List of possible target activities.
+    """
+
+    columns_list: Index[str] = None
+    """
+    List of dataset columns.
+    """
+
+    human_activities: Tuple[str] = ("har_bike", "har_bus", "har_car", "har_still", "har_walk")
+    """
+    List of human activity columns.
+    """
+
+    def get_instance(self) -> ActivityClassifier:
+        """
+        Returns an instance of this class.
+        """
+        if not ActivityClassifier.__instance:
+            ActivityClassifier.__instance = ActivityClassifier.__Classifier()
+            ActivityClassifier.columns_list = ActivityClassifier.__instance.train_dataset.columns
+
+        return self
     
     def update_retrain(self, new_record: pd.DataFrame, expected_target: np.ndarray) -> str:
         """
@@ -36,15 +53,22 @@ class Classifier:
         retrains the model and therefore predicts the class.
         """
         # Updating the training set and caching it again.
-        Classifier.__instance.train_dataset = Classifier.__instance.train_dataset.append(new_record)
-        Classifier.__instance.train_dataset.to_csv(self.train_dataset_filename)
+        ActivityClassifier.__instance.train_dataset = ActivityClassifier.__instance.train_dataset.append(new_record)
+        ActivityClassifier.__instance.train_dataset.to_csv(self.train_dataset_filename)
         
     
     def predict(self, new_record: pd.DataFrame) -> str:
         """
         Predicts the target class for the given new_record.
         """
-        prediction_result = Classifier.__instance.model.predict(new_record)
-        predicted_class_index = np.argmax(prediction_result)
+        # One-hot encoding of the har column.
+        ha_cols = pd.get_dummies(new_record["human_activity"], prefix="human_activity")
+        new_record.drop(["human_activity"], axis=1, inplace=True)
+        X = pd.concat([new_record, ha_cols], axis=1)
 
-        return Classifier.targets_list[predicted_class_index]
+        # Prediction
+        y = ActivityClassifier.__instance.model.predict(X)
+        predicted_class_index = np.argmax(y)
+
+        return ActivityClassifier.targets_list[predicted_class_index]
+        
