2fcb84b24a4e179d450216d6f8094a3802c8fbaa
TommasoAzz <azzalintommaso@gmail.com>
2021-10-05 21:29:46 +0200

    Fix sintassi

--- context_aware/contextaware/__main__.py
+++ context_aware/contextaware/__main__.py
@@ -9,35 +9,35 @@ import pandas as pd
 app = Flask(__name__)
 classifier = ActivityClassifier()
 
-def create_record(request) -> pd.DataFrame:
-        latitude = request.args.get('latitude')
-        longitude = request.args.get('longitude')
-        human_activity = request.args.get('human_activity')
-        date_time = request.args.get('date_time')
-        date_time = datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S%z')
-        seconds = (date_time.hour * 3600) + (date_time.minute * 60) + date_time.second
-        # 0 = Monday, 6 = Sunday
-        day_of_week = date_time.weekday()
-        
-        # New record creation
-        new_record = pd.DataFrame(columns=[
-            'place_lat', 'place_lon', 'time_of_day', 'day_of_week', 'human_activity_bike',
-            'human_activity_bus', 'human_activity_car', 'human_activity_still', 'human_activity_walk'
-            ])
-        
-        har_bike = "bike" == human_activity
-        har_bus = "bus" == human_activity
-        har_car = "car" == human_activity
-        har_still = "still" == human_activity
-        har_walk = "walk" == human_activity
+def create_record(req: request) -> pd.DataFrame:
+    latitude = req.args.get('latitude')
+    longitude = req.args.get('longitude')
+    human_activity = req.args.get('human_activity')
+    date_time = req.args.get('date_time')
+    date_time = datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S%z')
+    seconds = (date_time.hour * 3600) + (date_time.minute * 60) + date_time.second
+    # 0 = Monday, 6 = Sunday
+    day_of_week = date_time.weekday()
+    
+    # New record creation
+    new_record = pd.DataFrame(columns=[
+        'place_lat', 'place_lon', 'time_of_day', 'day_of_week', 'human_activity_bike',
+        'human_activity_bus', 'human_activity_car', 'human_activity_still', 'human_activity_walk'
+        ])
+    
+    har_bike = "bike" == human_activity
+    har_bus = "bus" == human_activity
+    har_car = "car" == human_activity
+    har_still = "still" == human_activity
+    har_walk = "walk" == human_activity
 
-        new_record.loc[0] = [latitude, longitude, seconds, day_of_week, har_bike, har_bus, har_car, har_still, har_walk]
-        return new_record
+    new_record.loc[0] = [latitude, longitude, seconds, day_of_week, har_bike, har_bus, har_car, har_still, har_walk]
+    return new_record
     
 
 
 @app.route("/recommendation/accuracy", methods=["GET"])
-def test_model():
+def compute_model_accuracy():
     """
     Returns the current accuracy of the model.
     """
@@ -45,8 +45,9 @@ def test_model():
 
     return test_result, 200
 
+
 @app.route("/recommendation/places", methods=["GET"])
-def recommend_places():
+def recommend_place_category():
     """
     Returns the recommended places (points of interest) to visit at the given parameters found as arguments.
     """    
@@ -62,7 +63,7 @@ def recommend_places():
 
 
 @app.route("/recommendation/validity", methods=["GET"])
-def validity():
+def should_advise_place_category():
     """
     Returns whether the user should perform or not certain activities given a possible activity.
     """
@@ -78,6 +79,7 @@ def validity():
     }
     return response, 200
 
+
 @app.route("/recommendation/train", methods=["POST"])
 def train_again_model():
     """
@@ -88,7 +90,7 @@ def train_again_model():
     latitude = request.args.get('latitude')
     longitude = request.args.get('longitude')
     human_activity = request.args.get('human_activity')
-    date_time = request.args.get('date_time')
+    date_time = request.args.get('date_time')  # TODO convert to millisecs
     place_category = request.args.get('place_category')
 
     # Arguments preprocessing
